5fb1d98ba69a7cded37daacab66ac28b7392990f
Marlon Krüger <s91642@bht-berlin.de>
2025-11-25 10:59:41 +0100

    feat: Bestätigungsseite vor Terminbuchung

--- frontend/src/router.d.ts
+++ frontend/src/router.d.ts
@@ -1,14 +1,19 @@
 import '@tanstack/react-router'
 import type {
+  AnimalsType,
   AppointmentsType,
+  ServiceType,
   VeterinaryPracticesType,
 } from '../../shared/schemas/ZodSchemas'
 
 // erweitert History State, damit Praxis auch als State uebergeben werden kann
 declare module '@tanstack/react-router' {
   interface HistoryState {
-    praxis?: VeterinaryPracticesType
+    praxis?: VeterinaryPracticesType // TODO changed to practice
+    practice?: VeterinaryPracticesType,
     termin?: AppointmentsType //TODO changed to appointment
     appointment?: AppointmentsType
+    selectedAnimal?: AnimalsType,
+    selectedService?: ServiceType,
   }
 }
