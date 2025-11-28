9c40dc2a63fcfa66c5e4246787c01b9455c5a321
Edgar Frank <edfr8687@bht-berlin.de>
2025-11-28 11:36:11 +0100

    Fixed seed issue

--- backend/src/routes/persons.ts
+++ backend/src/routes/persons.ts
@@ -40,13 +40,7 @@ personsRouter.get("/:id/favorites",
 personsRouter.post("/",
     async (req, res) => {
         try {
-            let unsafePerson = req.body;
-            unsafePerson = {
-                ...unsafePerson,
-                dateofbirth: new Date(unsafePerson.dateofbirth)
-            }
-
-            const personData = PersonsCreateSchema.safeParse(unsafePerson);
+            const personData = PersonsCreateSchema.safeParse(req.body);
             if (!personData.success) {
                 res.status(400).send(personData.error);
                 return;
