636e911a0ead0ff2d9de42da2bd57eb8be2e9e43
Bertan Bahcivan <beba3606@bht-berlin.de>
2025-12-04 12:26:11 +0100

    save conflict resolved

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,15 +8,8 @@ import { animalsRouter } from './routes/animals';
 import { animaltypeRouter } from './routes/animaltypes';
 import { serviceRouter } from './routes/services';
 import { loginRouter } from './routes/login';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { routerExceptionHandler } from './exceptions/routerExceptionMiddleware';
-=======
 import { emailverificationRouter } from './routes/emailverification';
->>>>>>> 28b79f1 (email service and router done needs to be tested though)
-=======
-import { emailverificationRouter } from './routes/emailverification';
->>>>>>> daa4e66b7b1f66d5978f35b5f380eda1f48f5352
 
 export const app = express();
 
@@ -32,12 +25,5 @@ app.use("/api/persons", personsRouter);
 app.use("/api/animals", animalsRouter);
 app.use("/api/animaltypes", animaltypeRouter);
 app.use("/api/services", serviceRouter);
-<<<<<<< HEAD
-<<<<<<< HEAD
 app.use(routerExceptionHandler);
-=======
 app.use("api/email-confirmation", emailverificationRouter);
->>>>>>> 28b79f1 (email service and router done needs to be tested though)
-=======
-app.use("api/email-confirmation", emailverificationRouter);
->>>>>>> daa4e66b7b1f66d5978f35b5f380eda1f48f5352
